65ab48f6c177806e260fb90b8521f397a004c72d
Mohsen Robatjazi <mohroba@gmail.com>
2025-08-24 15:17:49 +0330

    + Solve Style Issues

--- includes/Forms/ClubRegisterForm.php
+++ includes/Forms/ClubRegisterForm.php
@@ -112,7 +112,7 @@ class ClubRegisterForm extends BaseForm {
         ob_start();
         ?>
         <div class="club-form-container">
-            <div class="sd-header" style="margin-bottom: 15px">درخواست ثبت باشگاه</div>;
+            <div class="sd-header" style="margin-bottom: 15px">درخواست ثبت باشگاه</div>
             <?= $this->error_list(); ?>
             <form method="post" enctype="multipart/form-data" id="club-form">
                 <?php wp_nonce_field( $this->nonce_action, $this->nonce_name ); ?>
