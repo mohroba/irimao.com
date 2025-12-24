6721aadd51a7a51d406509ae6d666116daa862c6
Mohsen Robatjazi <25632948+mohroba@users.noreply.github.com>
2025-12-24 12:48:10 +0330

    Fix city representative form workflow and asset paths

--- assets/js/city-rep-form.js
+++ assets/js/city-rep-form.js
@@ -3,12 +3,29 @@ jQuery(function ($) {
   const $name = $('#city_user_name');
   const $userId = $('#city_user_id');
   const $checkBtn = $('#city_national_check');
+  const $citySelect = $('#city_name');
+  const $genderSelect = $('#city_gender');
+  const $submitBtn = $('#city_rep_submit');
 
   if (!$national.length || typeof IMAOCityRep === 'undefined') {
     return;
   }
 
   $('.crm-select2').select2({ width: 'resolve' });
+
+  const toggleFormState = (enabled) => {
+    $citySelect.prop('disabled', !enabled).trigger('change.select2');
+    $genderSelect.prop('disabled', !enabled).trigger('change.select2');
+    $submitBtn.prop('disabled', !enabled);
+  };
+
+  toggleFormState(!!$userId.val());
+
+  $national.on('input', function () {
+    $name.val('');
+    $userId.val('');
+    toggleFormState(false);
+  });
 
   $checkBtn.on('click', function (e) {
     e.preventDefault();
@@ -25,14 +42,17 @@ jQuery(function ($) {
       if (res && res.success) {
         $name.val(res.data.name || '');
         $userId.val(res.data.user_id || '');
+        toggleFormState(true);
       } else {
         $name.val('');
         $userId.val('');
+        toggleFormState(false);
         alert(res && res.data && res.data.message ? res.data.message : 'کاربر با این کد ملی یافت نشد.');
       }
     }).fail(function () {
       $name.val('');
       $userId.val('');
+      toggleFormState(false);
       alert('خطا در برقراری ارتباط. دوباره تلاش کنید.');
     });
   });
